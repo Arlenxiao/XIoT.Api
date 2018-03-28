9f618ccd991118ec69c65056929cf1953f207415
arlenxiao <arlenxiao@live.cn>
2018-03-29 00:48:21 +0800

    更新

---
+++ XIoT.Api.Web/Areas/Device/Controllers/CommandController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace XIoT.Api.Web.Areas.Device.Controllers
+{
+    /// <summary>
+    /// 设备指令
+    /// </summary>
+    public class CommandController : ApiController
+    {
+        /// <summary>
+        /// 设备公共指令
+        /// </summary>
+        /// <param name="token">令牌</param>
+        /// <param name="did">设备编码</param>
+        /// <param name="cmd">设备指令</param>
+        /// <returns></returns>
+        public string Write(String token, String did, String cmd)
+        {
+            return "value";
+        }
+    }
+}
---
+++ XIoT.Api.Web/Controllers/User/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace XIoT.Api.Web.Controllers.User
+{
+    /// <summary>
+    /// 用户登录
+    /// </summary>
+    public class LoginController : ApiController
+    {
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <param name="user">帐号</param>
+        /// <param name="pass">密码</param>
+        /// <returns></returns>
+        public string Login(String user, String pass)
+        {
+            return "value";
+        }
+    }
+}
